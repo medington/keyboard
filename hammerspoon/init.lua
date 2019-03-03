fe5bb42f951627087568579439b2135360bdef51
Mark Edington <medington@gmail.com>
2019-03-02 21:09:05 -0800

    Disable bindings not being used

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -32,12 +32,13 @@ enableHotkeyForWindowsMatchingFilter = function(windowFilter, hotkey)
 end
 
 require('keyboard.control-escape')
+require('keyboard.super')
 require('keyboard.delete-words')
 require('keyboard.hyper')
-require('keyboard.markdown')
-require('keyboard.microphone')
-require('keyboard.panes')
-require('keyboard.super')
 require('keyboard.windows')
+--require('keyboard.markdown')
+--require('keyboard.microphone')
+-- Disable this since we don't use it and it breaks ctrl-K
+--require('keyboard.panes')
 
 hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
